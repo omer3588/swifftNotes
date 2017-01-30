36c7b6f3c9677721ab7bcf94e895c5632443dcc1
Ömer yiğitsoy <omeryigitsoy@Omers-MacBook-Pro.local>
2017-01-30 22:25:57 +0300

    Project commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Ömer yiğitsoy on 08/01/2017.
+//  Copyright © 2017 Ömer yiğitsoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
